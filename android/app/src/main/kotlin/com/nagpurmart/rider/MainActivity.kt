66ea3e3aaefc972f8fe06a591b20066152e7984b
Tushar3529-dev <tusharyadav3529@gmail.com>
2026-05-16 13:14:27 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/nagpurmart/rider/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.nagpurmart.rider
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/config/constant.dart
@@ -0,0 +1,6 @@
+const String appName = 'Nagpur Rider';
+const String packageName = 'com.nagpur.rider';
+const String domainBaseUrl = 'https://admin.nagpurmart.in';
+const String baseUrl = '$domainBaseUrl/api/delivery-boy/';
+const String deliveryZoneUrl = '$domainBaseUrl/api/';
+const String defaultTheme = 'light';
